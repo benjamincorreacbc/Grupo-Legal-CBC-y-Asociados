637235851c2de8f0579be6863d34763c6b0d16ee
benjamincorreacbc <benjamincorreacbc@gmail.com>
2025-10-29 00:56:21 -0300

    Crear config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.__GLCBC_SUPABASE_URL__ = 'https://focxelshnrrvanlnusqf.supabase.co';
+window.__GLCBC_SUPABASE_ANON_KEY__ = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvY3hlbHNobnJydmFubG51c3FmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEzMTAyNDAsImV4cCI6MjA3Njg4NjI0MH0.VNFpA5hESELQpjKbhZPMPOIGJiX0mV5bJVg5FbtqH1s';
+window.__GLCBC_API_BASE__ = 'https://focxelshnrrvanlnusqf.supabase.co/functions/v1/portal';
+window.__GLCBC_ORG_SLUG__ = 'glcbc';
